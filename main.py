624204c55667112e2605656452ef9388f50a5e4c
Elena <femtochka@yahoo.com>
2022-10-25 18:12:35 +0300

    bot2

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-# from telegram import Update
-# from telegram.ext import Updater, CommandHandler, CallbackContext
 from bot_commands import *
-
-# updater = Updater('5739117950:AAFAPWiTqA_BXxUg69UO2Ba8KafvZNW9vKA')
-# updater.dispatcher.add_handler(CommandHandler('hi', hi_command))
-# updater.dispatcher.add_handler(CommandHandler('time', time_command))
-# updater.dispatcher.add_handler(CommandHandler('help', help_command))
-# updater.dispatcher.add_handler(CommandHandler('sum_int', sum_int_command))
-# updater.dispatcher.add_handler(CommandHandler('sum_float', sum_float_command))
-# print('server start')
-# updater.start_polling()
-# updater.idle()
-
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
